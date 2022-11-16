183cdc651a2847d287ecfedc80f668880b512486
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-11-16 18:15:00 +0700

    BE: Create category entity

--- backend/src/main/java/com/nashtech/assignment/data/entities/Category.java
+++ backend/src/main/java/com/nashtech/assignment/data/entities/Category.java
@@ -1,5 +1,23 @@
 package com.nashtech.assignment.data.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category_tbl")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+    @Column(name = "prefix_asset_code", unique = true)
+    private String prefixAssetCode;
 }
