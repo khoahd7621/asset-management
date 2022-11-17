f5a3fa4f45cc40dcc9e8b8ebcafcfaac1ff98fc1
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-11-17 11:16:08 +0700

    BE: Config CORS

--- backend/src/main/java/com/nashtech/assignment/config/CORSConfig.java
+++ backend/src/main/java/com/nashtech/assignment/config/CORSConfig.java
@@ -1,8 +1,26 @@
 package com.nashtech.assignment.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@Configurable
+import java.util.List;
+
+@Configuration
 public class CORSConfig {
-    
+    @Bean
+    public CorsFilter corsFilter(@Value("${spring.graphql.cors.allowed-origins}") List<String> allowedOrigins) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOriginPatterns(allowedOrigins);
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 }
